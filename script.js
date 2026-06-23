032e3b18eeb765d1363928674fcf471e12eb0e69
Dirty-Hippy <stephanie.s.panayides@gmail.com>
2026-06-23 13:42:08 -0700

    Added a different landing place and added leaf button

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// let image = document.querySelector("#inego");
+// let leaf = document.querySelector(".leaf");
+let textImg = document.querySelector(".textImg");
+
+
+
+// leaf.addEventListener("click", function(){
+//     window.location.replace("aboutme.html");
+//     console.log("click click");
+
+// });
+textImg.addEventListener("click", function(){
+    window.location.replace("aboutme.html");
+    console.log("click click");
+})
