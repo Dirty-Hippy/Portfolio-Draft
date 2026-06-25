9e4c70cf7204d9a077e7aa3f8fd6509a0d3d1692
Dirty-Hippy <stephanie.s.panayides@gmail.com>
2026-06-25 08:11:07 -0700

    started on change season filter

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 // let image = document.querySelector("#inego");
 // let leaf = document.querySelector(".leaf");
 let textImg = document.querySelector(".textImg");
+let landingPage = document.querySelector(".landing");
+let changeSeasonButton = document.querySelector(".changeSeasonButton");
+// let leafBox = document.querySelectoAll(".leafBox");
+function changeSeason(){
+    landingPage.style.filter="hue-rotate(100deg)";
+}
 
 
 
@@ -10,6 +16,8 @@ let textImg = document.querySelector(".textImg");
 //     console.log("click click");
 
 // });
+
+
 textImg.addEventListener("click", function(){
     window.location.replace("aboutme.html");
     console.log("click click");
